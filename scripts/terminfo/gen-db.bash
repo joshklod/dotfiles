8bf7dcbf8ea9fa2a1a26607e751baac86b5c087b
Josh Klodnicki <joshklod@gmail.com>
2019-02-07 12:51:14 -0500

    Terminfo: Fix installation
    Problem: Symlinks in database were not replaced by regular files
    
    Break installation into a two-step process.  'gen-db.bash' outputs to a
    clean directory in `scripts/terminfo`.  'install' runs 'gen-db.bash',
    then copies the whole thing to the destination using `cp
    --remove-destination`.

--- scripts/terminfo/gen-db.bash
+++ scripts/terminfo/gen-db.bash
@@ -5,8 +5,9 @@
 script_dir=$(dirname "$0")
 
 cd "$script_dir"
+rm -rf out
 
 while IFS= read -rd $'\0'; do
 	printf "Compiling '%s'...\n" "$script_dir/$REPLY"
-	tic -xo ~/.terminfo "$REPLY"
+	tic -xo out "$REPLY"
 done < <(find src -name '*.ti' -print0)
