a1d208eaa4cf8cfc99aa96dcd2c3563c6f013ba1
Josh Klodnicki <joshklod@gmail.com>
2019-02-07 10:56:15 -0500

    Terminfo: Use '.ti' extension for src files

--- scripts/terminfo/gen-db.bash
+++ scripts/terminfo/gen-db.bash
@@ -9,4 +9,4 @@ cd "$script_dir"
 while IFS= read -rd $'\0'; do
 	printf "Compiling '%s'...\n" "$script_dir/$REPLY"
 	tic -xo ~/.terminfo "$REPLY"
-done < <(find src -name '*.terminfo' -print0)
+done < <(find src -name '*.ti' -print0)
