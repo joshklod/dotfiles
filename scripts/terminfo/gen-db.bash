fe382c797abc62e806e8d1dce5a41c81580dde11
Josh Klodnicki <joshklod@gmail.com>
2019-02-07 15:24:17 -0500

    Terminfo: Fix compilation script
    Compile all files in one call to `tic` to guarantee internal resolution
    of `use=` dependencies

--- scripts/terminfo/gen-db.bash
+++ scripts/terminfo/gen-db.bash
@@ -8,6 +8,6 @@ cd "$script_dir"
 rm -rf out
 
 while IFS= read -rd $'\0'; do
-	printf "Compiling '%s'...\n" "$script_dir/$REPLY"
-	tic -sxo out "$REPLY"
-done < <(find src -name '*.ti' -print0)
+	printf "Compiling '%s'...\n" "$script_dir/$REPLY" >/dev/stderr
+	cat "$REPLY"
+done < <(find src -name '*.ti' -print0) | tic -sxo out -
