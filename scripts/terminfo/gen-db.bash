132cf425fa9dc2a1abeb8d8f30b083c98997d19b
Josh Klodnicki <joshklod@gmail.com>
2019-09-03 15:18:31 -0400

    Terminfo: gen-db--Make script portable

--- scripts/terminfo/gen-db.bash
+++ scripts/terminfo/gen-db.bash
@@ -2,11 +2,11 @@
 #
 # gen-db.bash - Generate terminfo database
 
-xargs-n () {
+xargs_n () {
 	tr '\n' '\0' | xargs -0 "$@"
 }
 
-script_dir=$(dirname "$0")
+script_dir=`dirname "$0"`
 
 cd "$script_dir"
 rm -rf out
@@ -14,7 +14,7 @@ rm -rf out
 files=`find src -name '*.ti'`
 
 printf 'Compiling:'
-echo "$files" | xargs-n printf " '%s'"
+echo "$files" | xargs_n printf " '%s'"
 printf '\n'
 
-echo "$files" | xargs-n cat | tic -sxo out -
+echo "$files" | xargs_n cat | tic -sxo out -
