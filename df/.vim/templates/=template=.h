e748c755253e26e3c5ea842c6be34d590ed5b9d5
Josh Klodnicki <joshklod@gmail.com>
2020-01-13 08:31:58 -0500

    Vim: Add templates for c files

---
+++ df/.vim/templates/=template=.h
@@ -0,0 +1,16 @@
+/**
+ * \file      %FFILE%
+ * \brief     %HERE%TODO
+ * \author    Josh Klodnicki
+ * \date      Created:  %DATE%
+ * \copyright (c) %YEAR% Josh Klodnicki.
+ *            Distributed under terms of the %LICENSE% license.
+ */
+
+#ifndef %GUARD%
+#define %GUARD%
+
+
+#endif // %GUARD%
+
+// vim: ft=c.doxygen
