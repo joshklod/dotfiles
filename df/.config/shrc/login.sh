3200f7ce11ce8a266cc172796bbea686d6976638
Josh Klodnicki <joshklod@gmail.com>
2026-05-08 13:36:33 -0400

    sh: Point ENV directly to .shrc.sh

--- df/.config/shrc/login.sh
+++ df/.config/shrc/login.sh
@@ -1,5 +1,5 @@
 # Make non-login POSIX shells work with shrc
-export ENV="$HOME/.env.sh"
+export ENV="$HOME/.shrc.sh"
 
 # Make non-interactive non-login Bash shells work with shrc
 export BASH_ENV="$HOME/.bashrc"
