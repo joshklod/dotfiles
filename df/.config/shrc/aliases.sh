514a916d533257a4ec2b3a9f8dd4afce9447b021
Josh Klodnicki <joshklod@gmail.com>
2026-05-11 12:47:50 -0400

    shrc: Move compatible Bash init to POSIX sh

---
+++ df/.config/shrc/aliases.sh
@@ -0,0 +1,61 @@
+## Preferences
+# Command defaults
+alias ls='ls -C --color=auto --dereference-command-line \
+          --group-directories-first --human-readable'
+alias grep='grep --color=auto'
+alias picocom='picocom -b 19200'
+alias bc='bc -l'
+
+# Interactive overwriting
+alias rm='rm -I'
+alias cp='cp -i'
+alias mv='mv -i'
+
+## Shortcuts
+# ls all
+alias la='ls --almost-all'
+# ls list
+alias ll='ls -g --no-group'
+# ls all list
+alias lal='ls -g --almost-all --no-group'
+# ls long list
+alias lll='ls -l'
+# ls all long list
+alias lall='ls -l --all'
+alias llal='lall'
+
+# Go back to previous directory
+alias back='cd "$OLDPWD"'
+
+# Print $PATH in a human-readable format
+alias path='echo $PATH | tr : \\n'
+
+# Enjoy!
+alias lulz='cat /dev/urandom | hexdump -C |
+            GREP_COLORS="mt=1;32" grep -E "[[:xdigit:]]{4}0000"'
+
+# mkdir and cd
+mkcd() { mkdir "$1" && cd "$1"; }
+
+case "$(uname -s)" in
+	CYGWIN*)
+		# Open Explorer window in current directory
+		alias exp='explorer .'
+
+		# Start X Server in the background silently
+		alias silentx='log --stderr startxwin'
+
+		# Automatically set $DISPLAY if unset
+		alias auto-disp='[ -z "$DISPLAY" ] && export DISPLAY=:0.0'
+		# Start X programs with default DISPLAY
+		alias x='auto-disp; silentx'
+		alias gvim='auto-disp; gvim'
+
+		# Executes command in a separate window
+		win () { mintty "$@" & }
+
+		# Windows GUI for updating Cygwin
+		alias cygupdate='curl -o "/proc/cygdrive/c/applications/cygwin-setup.exe" https://cygwin.com/setup-x86_64.exe'
+		alias cygupgrade='cygsetup --quiet-mode'
+		;;
+esac
---
+++ df/.config/shrc/interactive.sh
@@ -0,0 +1,54 @@
+## Script utilities
+
+# Shortcut to check command existence
+iscommand () { command -v "$@" >/dev/null 2>&1; }
+
+# Terminal-specific hacks
+if [ -n "$VIM_TERMINAL" ]; then
+	# If the parent terminal supports truecolor, use the truecolor syntax that
+	# Vim understands.  If not, leave $TERM alone.  Programs won't send
+	# truecolor sequences, and Vim will handle everything correctly.
+	if tput truecolor 2>/dev/null; then
+		export TERM=xterm-semitruecolor # Vim's terminal acts like this
+	fi
+	unset VIM_TERMINAL # This is misleading if inherited by another terminal
+elif [ -n "$WT_SESSION" ]; then
+	# Windows Terminal
+	export TERM=xterm-truecolor
+fi
+
+# Check terminal for color support
+if iscommand tput; then
+	COLORS=$(tput colors) || COLORS=-1
+	if tput truecolor 2>/dev/null; then
+		export COLORTERM=truecolor
+	else
+		unset COLORTERM
+	fi
+else
+	COLORS=-1
+fi
+export COLORS
+
+# Environment variables
+export AUTOPAGE_CUTOFF='50%'
+export EDITOR=$(command -v vim) # Use Vim as default editor
+export GNUMAKEFLAGS='--output-sync=target --no-print-directory'
+export HISTCONTROL=ignoredups # Ignore duplicates in history
+export LESS='-iR'               # Interpret ANSI escape sequences
+export MAKEFLAGS='--jobs=4'
+
+# LS Colors
+if [ $COLORS -ge 8 ] && iscommand dircolors; then
+	if [ -f "$HOME/.dircolors" ]; then
+		eval $(dircolors -b "$HOME/.dircolors")
+	else
+		eval $(dircolors -b)
+	fi
+fi
+
+## Source aliases file
+[ -f "$SHRC_DIR/aliases.sh" ] && . "$SHRC_DIR/aliases.sh"
+
+## Cleanup
+unset iscommand
